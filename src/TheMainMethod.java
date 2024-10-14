c8da9f7165b8b528029dc4400580e8e42a28f718
QJohnson321 <98294763+QJohnson321@users.noreply.github.com>
2024-10-13 21:16:12 -0400

    Update TheMainMethod.java

--- src/TheMainMethod.java
+++ src/TheMainMethod.java
@@ -1,4 +1,12 @@
-// Starting the program
+/*
+Quintin Johnson
+Oct 13, 2024
+Cen 3024
+
+This Starts the program
+
+ */
+
 public class TheMainMethod {
     public static void main(String[] args) {
         MainMenu calling = new MainMenu();
